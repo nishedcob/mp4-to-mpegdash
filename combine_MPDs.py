15f199bb510282fe9ab48df26c31a06d3ca4c2c4
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2018-04-11 11:38:41 -0500

    Prototype Python Script that combines generated MPDs

---
+++ combine_MPDs.py
@@ -0,0 +1,64 @@
+# coding: utf-8
+
+import xmltodict
+from collections import OrderedDict
+
+#############################################
+
+mpd_name = "WonderWoman2017_10min_dash.mpd"
+video_res = ['320', '640', '720', '1280', '1920', '2560']
+audio_trk = ['audio']
+
+#############################################
+
+print("Video MPDs:")
+for v_res in video_res:
+    print("%s/%s" % (v_res, mpd_name))
+
+print("Audio MPDs:")
+for a_trk in audio_trk:
+    print("%s/%s" % (a_trk, mpd_name))
+
+############################################
+
+v_res = video_res[-1]
+
+cur_mpd = "%s/%s" % (v_res, mpd_name)
+
+internal_dict = xmltodict.parse(open(cur_mpd).read())
+
+del internal_dict['MPD']['Period']['AdaptationSet']['Representation']
+internal_dict['MPD']['Period']['AdaptationSet']['Representation'] = []
+
+for v_res in video_res:
+    cur_mpd = "%s/%s" % (v_res, mpd_name)
+    print(cur_mpd)
+    internal_dict['MPD']['Period']['AdaptationSet']['Representation']\
+        .append(
+            xmltodict.parse(open(cur_mpd).read())\
+                ['MPD']['Period']['AdaptationSet']['Representation']
+        )
+    internal_dict['MPD']['Period']['AdaptationSet']['Representation'][-1]\
+        ['BaseURL'] = "%s/" % v_res
+
+adaptation_set = internal_dict['MPD']['Period']['AdaptationSet']
+del internal_dict['MPD']['Period']['AdaptationSet']
+internal_dict['MPD']['Period']['AdaptationSet'] = [adaptation_set]
+
+a_trk = audio_trk[-1]
+a_trk_file = "%s/%s" % (a_trk, mpd_name)
+print(a_trk_file)
+
+internal_dict['MPD']['Period']['AdaptationSet'].append(
+    xmltodict.parse(open(a_trk_file).read())['MPD']['Period']['AdaptationSet']
+)
+
+xml_out = xmltodict.unparse(internal_dict, pretty=True)
+
+print(xml_out)
+
+out_file_name = mpd_name
+
+out_file = open(out_file_name, 'w')
+out_file.write(xml_out)
+out_file.close()
