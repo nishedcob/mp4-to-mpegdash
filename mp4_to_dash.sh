4dec30d281f4ea376bdfa16a2af2e1ce57206ab7
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2018-04-18 10:42:50 -0500

    Add argument support to mp4_to_dash.sh

--- mp4_to_dash.sh
+++ mp4_to_dash.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
-perl transcode.pl WonderWoman2017_10min.mp4 ; pipenv run python combine_MPDs.py
+echo "Usage: $0 name_of_mp4_without.ext"
 
+echo 'perl transcode.pl' "$1.mp4"
+perl transcode.pl "$1.mp4"
 
+echo 'pipenv run python combine_MPDs.py --mpd_name' "$1_dash.mpd"
+pipenv run python combine_MPDs.py --mpd_name "$1_dash.mpd"
